ff3c69afe01858f87eb4db9190a9e48dc5d93519
bethel-m <bethelcletus87@gmail.com>
2024-12-27 12:43:55 +0100

    Deploy to GitHub Pages: 2024-12-27 12:43:55

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cc6a6ae15426d336cedaf6cea382b122",
+const RESOURCES = {"flutter_bootstrap.js": "419fb7e46233ffc3a25d8b8a2795dc59",
 "index.html": "ddfc3b404abd2d9039e19d5941c489dd",
 "/": "ddfc3b404abd2d9039e19d5941c489dd",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -26,7 +26,7 @@ const RESOURCES = {"flutter_bootstrap.js": "cc6a6ae15426d336cedaf6cea382b122",
 "assets/AssetManifest.bin.json": "e120fa585090bcee3850481c073647b8",
 "assets/fonts/MaterialIcons-Regular.otf": "a64b1520979508c8eba50497ae342f67",
 "assets/NOTICES": "2111c995ec98e4116bb7529eb0c6f4cb",
-"main.dart.js": "cd9b4668514fe5f68ff1d1696c828d1c",
+"main.dart.js": "8c498eb8287da7928b1275a4bbcf3b80",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "manifest.json": "004cb36544325fffaf106c957f850a4a",
 "canvaskit/skwasm.js.symbols": "c3c05bd50bdf59da8626bbe446ce65a3",
